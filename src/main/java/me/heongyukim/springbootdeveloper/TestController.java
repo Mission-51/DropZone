71bdfaef647eda24ecc67b6b4a48169202fe1797
김헌규 <khg6436@naver.com>
2024-08-29 00:49:08 +0900

    no-nginx-DB-cicd-test

--- src/main/java/me/heongyukim/springbootdeveloper/TestController.java
+++ src/main/java/me/heongyukim/springbootdeveloper/TestController.java
@@ -7,6 +7,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test() {
-        return "Hello, world!!!@@@@";
+        return "Hello, world!!!@@@@ 찐막 CICD 테스트...";
     }
 }
