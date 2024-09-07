cf723cb3b85e50f0faacee515ecf8d864d4a9187
김헌규 <khg6436@naver.com>
2024-09-07 22:02:35 +0900

    ci/cd-test

--- server/src/main/java/me/heongyukim/springbootdeveloper/TestController.java
+++ server/src/main/java/me/heongyukim/springbootdeveloper/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test() {
-        return "Hello, world!!!@@@@ 찐막 CICD 테스트...황준 멍청이" +
+        return "제발되라...." +
                 "황준, 손동희, 김동준, 차상곤, 박재영, 김헌규 레츠고!";
     }
 }
