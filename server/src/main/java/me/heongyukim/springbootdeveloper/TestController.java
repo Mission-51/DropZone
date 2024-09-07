09488564cf1487b6d72d1107a1b39b55512aafc5
김헌규 <khg6436@naver.com>
2024-09-07 22:24:41 +0900

    ci/cd-test6

--- server/src/main/java/me/heongyukim/springbootdeveloper/TestController.java
+++ server/src/main/java/me/heongyukim/springbootdeveloper/TestController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class TestController {
     @GetMapping("/test")
     public String test() {
-        return "제발되라....제발제발제발제발다시" +
+        return "제발되라....제발제발제발제발다시다시" +
                 "황준, 손동희, 김동준, 차상곤, 박재영, 김헌규 레츠고!";
     }
 }
