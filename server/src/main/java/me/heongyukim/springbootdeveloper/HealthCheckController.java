b0c32c865b16b3e8277e41c7284e42921dcdc3c2
김헌규 <khg6436@naver.com>
2024-09-09 22:34:55 +0900

    Update server/src/main/java/me/heongyukim/springbootdeveloper/HealthCheckController.java

--- server/src/main/java/me/heongyukim/springbootdeveloper/HealthCheckController.java
+++ server/src/main/java/me/heongyukim/springbootdeveloper/HealthCheckController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
-    @GetMapping("/health-check")
+    @GetMapping("/api/health-check")
     public String healthCheck() {
         return "OK";
     }
