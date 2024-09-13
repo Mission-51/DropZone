2a74f223efabe77c5d07c2d17c022676ef29b31d
김헌규 <khg6436@naver.com>
2024-09-13 10:48:10 +0900

    Update server/src/test/java/io/security/springsecuritymaster/SpringSecurityMasterApplicationTests.java

--- server/src/test/java/io/security/springsecuritymaster/SpringSecurityMasterApplicationTests.java
+++ server/src/test/java/io/security/springsecuritymaster/SpringSecurityMasterApplicationTests.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-@TestPropertySource(properties = {
-    "jasypt.encryptor.password=Dr@pZoneKey2024!@#$"
-})
 class SpringSecurityMasterApplicationTests {
 
     @Test
