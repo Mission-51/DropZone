a83cac71ebea05931a27c5f76863d8d434a22c8d
김헌규 <khg6436@naver.com>
2024-09-13 14:08:49 +0900

    healthCheckController-add

--- server/src/main/java/com/dropzone/cicdCheck/controller/HealthCheckController.java
+++ server/src/main/java/com/dropzone/cicdCheck/controller/HealthCheckController.java
@@ -1,4 +1,4 @@
-package me.heongyukim.springbootdeveloper;
+package com.dropzone.cicdCheck.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
