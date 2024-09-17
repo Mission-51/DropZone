9646eb130689c80fe7b676a2a98e359946136e81
김헌규 <khg6436@naver.com>
2024-09-17 14:11:19 +0900

    health-check-test

--- server/src/main/java/com/dropzone/HealthCheckController.java
+++ server/src/main/java/com/dropzone/HealthCheckController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HealthCheckController {
-    @GetMapping("/api/health-check")
+    @GetMapping("/health-check")
     public String healthCheck() {
         return "OK";
     }
