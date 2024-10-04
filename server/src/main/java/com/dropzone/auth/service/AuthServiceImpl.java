5d25af0b29bf12ea48f104e34c42303c0ae7c3bf
김헌규 <khg6436@naver.com>
2024-10-04 14:44:30 +0900

    hotfix: 중복 로그인 방지 로직  ì ì삭제

--- server/src/main/java/com/dropzone/auth/service/AuthServiceImpl.java
+++ server/src/main/java/com/dropzone/auth/service/AuthServiceImpl.java
@@ -34,8 +34,8 @@ public class AuthServiceImpl implements AuthService {
             if (passwordEncoder.matches(password, userEntity.getUserPassword())) {
 
                 // 로그인 성공 시 is_online 값을 true로 설정
-                userEntity.setUserIsOnline(true);
-                userRepository.save(userEntity);
+//                userEntity.setUserIsOnline(true);
+//                userRepository.save(userEntity);
 
                 // 비밀번호가 일치할 경우 UserEntity를 UserDTO로 변환하여 반환
                 return toUserDTO(userEntity);
@@ -49,21 +49,21 @@ public class AuthServiceImpl implements AuthService {
         }
     }
 
-    @Override
-    public void logout(String email) {
-        // 이메일로 사용자 찾기
-        Optional<UserEntity> loginUserEntity = userRepository.findByUserEmail(email);
-
-        // 사용자가 존재할 경우
-        if (loginUserEntity.isPresent()) {
-            UserEntity userEntity = loginUserEntity.get();
-
-            // 로그아웃 처리: is_online을 false로 설정
-            userEntity.setUserIsOnline(false);
-            userRepository.save(userEntity); // DB에 업데이트
-        } else {
-            // 사용자가 존재하지 않을 경우 예외 발생
-            throw new RuntimeException("Invalid email");
-        }
-    }
+//    @Override
+//    public void logout(String email) {
+//        // 이메일로 사용자 찾기
+//        Optional<UserEntity> loginUserEntity = userRepository.findByUserEmail(email);
+//
+//        // 사용자가 존재할 경우
+//        if (loginUserEntity.isPresent()) {
+//            UserEntity userEntity = loginUserEntity.get();
+//
+//            // 로그아웃 처리: is_online을 false로 설정
+//            userEntity.setUserIsOnline(false);
+//            userRepository.save(userEntity); // DB에 업데이트
+//        } else {
+//            // 사용자가 존재하지 않을 경우 예외 발생
+//            throw new RuntimeException("Invalid email");
+//        }
+//    }
 }
