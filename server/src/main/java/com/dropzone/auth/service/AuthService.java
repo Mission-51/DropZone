1b9968930a9eeb20b870a6cb817303c88cb0d5c7
차상곤 <yg9618@naver.com>
2024-10-04 10:56:13 +0900

    🚑️ 중복 로그인 수정
    중복 로그인 수정

--- server/src/main/java/com/dropzone/auth/service/AuthService.java
+++ server/src/main/java/com/dropzone/auth/service/AuthService.java
@@ -5,4 +5,7 @@ import com.dropzone.user.dto.UserDTO;
 public interface AuthService {
     // 이메일과 비밀번호를 사용하여 로그인을 처리하는 메서드
     public UserDTO loginByEmail(String email, String password);
+
+    // 로그아웃
+    public void logout(String email);
 }
