89332e909c02a0fa44022e9452bc7035082e8364
김헌규 <khg6436@naver.com>
2024-10-16 08:54:04 +0900

    hotfix: 로그인 중복 로직 업데이트
    Former-commit-id: 272898b0459f122121c4b36f3904ac5f36eae7bf

--- server/src/main/java/com/dropzone/auth/service/AuthService.java
+++ server/src/main/java/com/dropzone/auth/service/AuthService.java
@@ -6,6 +6,6 @@ public interface AuthService {
     // 이메일과 비밀번호를 사용하여 로그인을 처리하는 메서드
     public UserDTO loginByEmail(String email, String password);
 
-//    // 로그아웃
-//    public void logout(String userId);
+    // 로그아웃
+    public void logout(String userId);
 }
